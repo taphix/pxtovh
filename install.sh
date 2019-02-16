c6da1668e145f768bbd878867c92d2c0c28e58c2
taphix <33427932+taphix@users.noreply.github.com>
2019-02-16 22:41:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/taphix/pxtovh;
-echo 'alias pxtovh="pyhton3 /home/user/pxtovh/pxtovh.py"' >> ~/.bashrc;
+echo "alias pxtovh='python3 /home/$USER/pxtovh/pxtovh.py'" >> ~/.bashrc;
+echo "\033[90m[\033[92m+\033[90m]\033[93m Successfully installed\033[0m"
+echo "\033[90m[\033[92m+\033[90m]\033[93m Usage: \033[97mpxtovh\033[0m"
+echo "\033[90m[\033[92m+\033[90m]\033[93m enter 0 to exit\033[0m"
